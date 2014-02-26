9ad78af7b1dc96cf4983085e2884e87860f88204
vtortola <vtortola@gmail.com>
2014-02-26 12:28:10 +0000

    Creation

---
+++ TCPClient/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    public class Program
+    {
+        static IPEndPoint _endPoint;
+        static TimeSpan _delay;
+
+        public static void Main(String[] args)
+        {            
+            IPAddress address;
+            Int32 port;
+            Int32 amountOfClients;
+            Int32 millisecondDelay;
+
+            if(args.Length < 4 || !IPAddress.TryParse(args[0],out address) || !Int32.TryParse(args[1], out port) || !Int32.TryParse(args[2],out amountOfClients) || !Int32.TryParse(args[3], out millisecondDelay))
+            {
+                Console.WriteLine("parameters: [IPAddress] [Port] [Number of clients] [Delay between messages (ms)]");
+                return;
+            }
+
+            Thread.Sleep(1000); // give the server time to start up.
+            Console.Title = "Client";
+
+            _endPoint = new IPEndPoint(address, port);
+            _delay = TimeSpan.FromMilliseconds(millisecondDelay);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancel = cancellationTokenSource.Token;
+            Random ran = new Random(DateTime.Now.Millisecond);
+
+            List<Task> clientList = new List<Task>();
+            for (int i = 0; i < amountOfClients; i++)
+            {
+                clientList.Add(CreateClientAsync(cancel, ran, amountOfClients));
+            }
+                                    
+            Console.ReadKey();
+            cancellationTokenSource.Cancel();
+            Task.WaitAll(clientList.ToArray());
+            Console.WriteLine("end");
+            Console.ReadKey(true);
+        }
+
+        private static async Task CreateClientAsync(CancellationToken cancel, Random random, Int32 amountOfClients)
+        {
+            await Task.Yield();
+
+            while (!cancel.IsCancellationRequested)
+            {
+                Console.WriteLine("Connecting " + _endPoint.ToString());
+                await Task.Delay(random.Next(10, amountOfClients));
+
+                StreamReader sr = null;
+                StreamWriter sw = null;
+                try
+                {
+                    TcpClient client = new TcpClient();
+                    await client.ConnectAsync(_endPoint.Address, _endPoint.Port);
+                    var stream = client.GetStream();
+                    sr = new StreamReader(stream, Encoding.UTF8);
+                    sw = new StreamWriter(stream, Encoding.UTF8);
+                    while (client.Connected && !cancel.IsCancellationRequested)
+                    {
+                        await sw.WriteLineAsync("- There's something very important I forgot to tell you. - What? - Don't cross the streams. - Why? - It would be bad. - I'm fuzzy on the whole good/bad thing. What do you mean, 'bad'? - Try to imagine all life as you know it stopping instantaneously and every molecule in your body exploding at the speed of light. - Total protonic reversal. - Right. That's bad. Okay. All right. Important safety tip. Thanks, Egon.");
+                        await sw.FlushAsync();
+                        var msg = await sr.ReadLineAsync();
+                        //Console.WriteLine("Server says " + msg);
+                        await Task.Delay(_delay);
+                    }
+                }
+                catch (Exception aex)
+                {
+                    var ex = aex.GetBaseException();
+                    Console.WriteLine("Client error: " + ex.Message);
+                }
+                finally
+                {
+                    if (sr != null)
+                        sr.Dispose();
+                    if (sw != null)
+                        sw.Dispose();
+                }
+                Console.WriteLine("Disconnecting " + _endPoint.ToString());
+            }
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class Program
+    {
+        static PerformanceCounter _inMessages, _inBytes, _outMessages, _outBytes, _connected;
+
+        public static void Main(String[] args)
+        {
+            if (CreatePerformanceCounters())
+                return;
+
+            Console.Title = "Server";
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancel = cancellationTokenSource.Token;
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            Console.WriteLine("Service listening at " + listener.LocalEndpoint.ToString());
+
+            var task = AcceptClientsAsync(listener, cancel);
+
+            Console.ReadKey();
+            cancellationTokenSource.Cancel();
+            task.Wait();
+            Console.WriteLine("end");
+            Console.ReadKey(true);
+        }
+
+        public static async Task AcceptClientsAsync(TcpListener listener, CancellationToken cancel)
+        {
+            await Task.Yield();
+
+            while (!cancel.IsCancellationRequested)
+            {
+                try
+                {
+                    var timeoutTask = Task.Delay(2000);
+                    var acceptTask = listener.AcceptTcpClientAsync();
+
+                    await Task.WhenAny(timeoutTask, acceptTask);
+                    if (!acceptTask.IsCompleted)
+                        continue;
+
+                    var client = await acceptTask;
+                    HandleClientAsync(client, cancel);
+                }
+                catch (Exception aex)
+                {
+                    var ex = aex.GetBaseException();
+                    Console.WriteLine("Accepting error: " + ex.Message);
+                }
+            }
+        }
+
+        public static async Task HandleClientAsync(TcpClient client, CancellationToken cancel)
+        {
+            await Task.Yield();
+            _connected.Increment();
+            var local = client.Client.LocalEndPoint.ToString();
+            Console.WriteLine("Connected " + local);
+            StreamReader sr = null;
+            StreamWriter sw = null;
+            try
+            {
+                var stream = client.GetStream();
+                sr = new StreamReader(stream, Encoding.UTF8);
+                sw = new StreamWriter(stream, Encoding.UTF8);
+                while (!cancel.IsCancellationRequested && client.Connected)
+                {
+                    var readTask = sr.ReadLineAsync();
+                    var timeoutTask = Task.Delay(5000);
+
+                    await Task.WhenAny(readTask, timeoutTask);
+                    if (!readTask.IsCompleted)
+                        continue;
+
+                    var msg = await readTask;
+
+                    if (msg == null)
+                        continue;
+
+                    _inMessages.Increment();
+                    _inBytes.IncrementBy(msg.Length);
+                    //Console.WriteLine("Client says: " + msg);
+                    await sw.WriteLineAsync(msg);
+                    await sw.FlushAsync();
+                    _outMessages.Increment();
+                    _outBytes.IncrementBy(msg.Length);
+                }
+            }
+            catch (Exception aex)
+            {
+                var ex = aex.GetBaseException();
+                Console.WriteLine("Client error: " + ex.Message);
+            }
+            finally
+            {
+                _connected.Decrement();
+
+                if (sr != null)
+                    sr.Dispose();
+                if (sw != null)
+                    sw.Dispose();
+            }
+            Console.WriteLine("Disconnected " + local);
+        }
+
+        static String msgIn = "Messages In /sec", byteIn = "Bytes In /sec", msgOut = "Messages Out /sec", byteOut = "Bytes Out /sec", connected = "Connected";
+        private static bool CreatePerformanceCounters()
+        {
+            string categoryName = "TcpListener_Test";
+
+            if (!PerformanceCounterCategory.Exists(categoryName))
+            {
+                var ccdc = new CounterCreationDataCollection();
+
+                ccdc.Add(new CounterCreationData
+                {
+                    CounterType = PerformanceCounterType.RateOfCountsPerSecond64,
+                    CounterName = msgIn
+                });
+
+                ccdc.Add(new CounterCreationData
+                {
+                    CounterType = PerformanceCounterType.RateOfCountsPerSecond64,
+                    CounterName = byteIn
+                });
+
+                ccdc.Add(new CounterCreationData
+                {
+                    CounterType = PerformanceCounterType.RateOfCountsPerSecond64,
+                    CounterName = msgOut
+                });
+
+                ccdc.Add(new CounterCreationData
+                {
+                    CounterType = PerformanceCounterType.RateOfCountsPerSecond64,
+                    CounterName = byteOut
+                });
+
+                ccdc.Add(new CounterCreationData
+                {
+                    CounterType = PerformanceCounterType.NumberOfItems64,
+                    CounterName = connected
+                });
+
+                PerformanceCounterCategory.Create(categoryName, "", PerformanceCounterCategoryType.SingleInstance, ccdc);
+
+                Console.WriteLine("Performance counters have been created, please re-run the app");
+                return true;
+            }
+            else
+            {
+                //PerformanceCounterCategory.Delete(categoryName);
+                //Console.WriteLine("Delete");
+                //return true;
+
+                _inMessages = new PerformanceCounter(categoryName, msgIn, false);
+                _inBytes = new PerformanceCounter(categoryName, byteIn, false);
+                _outMessages = new PerformanceCounter(categoryName, msgOut, false);
+                _outBytes = new PerformanceCounter(categoryName, byteOut, false);
+                _connected = new PerformanceCounter(categoryName, connected, false);
+                _connected.RawValue = 0;
+
+                return false;
+            }
+        }       
+
+    }
+}
